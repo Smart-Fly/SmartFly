336eefa89f3bd7d8d01ae1e5d8a8d72e4c458b85
unknown <azis6229@gmail.com>
2020-08-23 15:45:05 +0700

    layouting in folder client modified:   client/my-app/package-lock.json modified:   client/my-app/package.json modified:   client/my-app/public/favicon.ico modified:   client/my-app/public/index.html modified:   client/my-app/src/App.js +add new file in folder Pages new file:   client/my-app/src/Pages/Home.js new file:   client/my-app/src/Pages/Result.css new file:   client/my-app/src/Pages/Result.js new file:   client/my-app/src/Pages/airportList.json new file:   client/my-app/src/Pages/data.js new file:   client/my-app/src/Pages/style.css +add new file in folder Components new file:   client/my-app/src/components/CardResult.js new file:   client/my-app/src/components/Navbar.js +add image in folder src new file:   client/my-app/src/img/Garuda.jpg new file:   client/my-app/src/img/background.jpg new file:   client/my-app/src/img/emirat.jpg new file:   client/my-app/src/img/nature.jpg new file:   client/my-app/src/img/run.jpg new file:   client/my-app/src/img/wing1.jpg new file:   client/my-app/src/img/wing2.jpg new file:   client/my-app/src/img/wing3.jpg modified:   client/my-app/src/index.js

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Home from './Pages/Home'
+import Result from './Pages/Result'
+import { ApolloProvider } from '@apollo/client'
+import { BrowserRouter as Router, Route, NavLink, Switch } from 'react-router-dom'
+import Navbar from './components/Navbar'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      {/* <ApolloProvider client={client}>
+      </ApolloProvider> */}
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/res" component={Result} />
+        </Switch>
+      </Router>
     </div>
   );
 }
