568cef760bb2b2f717c6a9c4103ea148d1fa0c7e
heriant0 <herianto890@gmail.com>
2020-08-23 13:01:21 +0700

    delete folder serverScraper

--- serverScrapper/routes/index.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router()
-const flightControllers = require('../controllers/flightControllers')
-
-router.post('/flightPrice', flightControllers.getFlightData)
-
-module.exports = router;
