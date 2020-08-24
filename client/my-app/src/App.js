57e79f1b9ef9be47e6f7f339945c1888ffc44cf0
luqmanabyadh <lqmn.abydh@gmail.com>
2020-08-24 07:08:38 +0700

    Adding Severals Feature: ========= In folder client the changes are: ========= 	modified:   client/my-app/src/App.js 	new file:   client/my-app/src/Pages/LoginPage.js 	new file:   client/my-app/src/Pages/RegisterPage.js 	deleted:    client/my-app/src/Pages/Result.css 	deleted:    client/my-app/src/Pages/Result.js 	new file:   client/my-app/src/Pages/UpdatePage.js 	modified:   client/my-app/src/components/Navbar.js 	new file:   client/my-app/src/config/client.js 	new file:   client/my-app/src/query/userQuery.js ========= And In Folder Server the changes Are: ========= 	modified:   server/orchestrator/graphql/package-lock.json 	new file:   server/orchestrator/graphql/package.json 	modified:   server/orchestrator/graphql/schemas/userSchema.js 	new file:   server/orchestrator/tes.text 	modified:   server/services/flightprices/package.json 	modified:   server/services/predictions/test/prediction.test.js 	modified:   server/services/users/config/config.json 	modified:   server/services/users/controller/UserController.js 	modified:   server/services/users/middleware/errorHandler.js

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,23 +1,28 @@
-import React from 'react';
-import Home from './Pages/Home'
-import Result from './Pages/Result'
-import { ApolloProvider } from '@apollo/client'
-import { BrowserRouter as Router, Route, NavLink, Switch } from 'react-router-dom'
-import Navbar from './components/Navbar'
+import React from "react";
+// import Home from "./Pages/Home";
+import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import Navbar from "./components/Navbar";
+import RegisterPage from "./Pages/RegisterPage";
+import LoginPage from "./Pages/LoginPage";
+import UpdatePage from "./Pages/UpdatePage";
 
+import { client } from "./config/client";
 
 function App() {
   return (
-    <div >
-      {/* <ApolloProvider client={client}>
-      </ApolloProvider> */}
-      <Router>
-        <Navbar/>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/res" component={Result} />
-        </Switch>
-      </Router>
+    <div>
+      <ApolloProvider client={client}>
+        <Router>
+          {/* <Navbar /> */}
+          <Switch>
+            {/* <Route exact path="/" component={Home} /> */}
+            <Route exact path="/register" component={RegisterPage} />
+            <Route exact path="/login" component={LoginPage} />
+            <Route exact path="/update" component={UpdatePage} />
+          </Switch>
+        </Router>
+      </ApolloProvider>
     </div>
   );
 }
