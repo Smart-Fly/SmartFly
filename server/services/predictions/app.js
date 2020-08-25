7e8445ad23aa5931a48450fe5439136f44538a99
heriant0 <herianto890@gmail.com>
2020-08-25 10:25:42 +0700

    running prediction feature

--- server/services/predictions/app.js
+++ server/services/predictions/app.js
@@ -44,6 +44,7 @@ app.get("/pricePrediction/:departure/:arrival", (req,res) => {
           let data1 = Array.from(Array(data2.length),(_, index) => index + 1)
           const dataX = JSON.stringify(data1)
           const dataY = JSON.stringify(data2)
+
           var process = spawn('python3',["./helpers/machineLearning.py",dataX,dataY])
           process.stdout.on('data', function(data) {
             let accuracy = +data.toString().split(' ')[0]
