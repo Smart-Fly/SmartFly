ef577db8ff1f48f23f910854edda2e913257f230
luqmanabyadh <lqmn.abydh@gmail.com>
2020-08-26 00:32:25 +0700

    implementasi cache

--- client/my-app/src/config/config.js
+++ client/my-app/src/config/config.js
@@ -1,8 +1,25 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
+
+export const userCache = makeVar({});
 
 const client = new ApolloClient({
   uri: "http://localhost:4000",
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Person: {
+        // Every person type
+        fields: {
+          userLoginCache: {
+            // A field on the person type
+            read() {
+              // when it's read
+              return userCache();
+            },
+          },
+        },
+      },
+    },
+  }),
 });
 
 export default client;
